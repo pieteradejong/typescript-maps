208c00e660f0d837e85a1a041a9a1af02fa79e05
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-07 17:37:33 -0400

    add configs for typescript and api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+/// <reference types="@types/google.maps" />
+import config from "../config.json"
+import { Company } from './Company';
 import { User } from "./User";
 
 const user = new User();
+const company = new Company();
 
 console.log(user);
-
+console.log(company);
